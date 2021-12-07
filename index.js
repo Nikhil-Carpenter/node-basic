77ba532519581ac1a42becae3328a32cf2df49be
Nikhil-Carpenter <nikhilcrpntr222@gmail.com>
2021-12-07 20:11:07 +0530

    index js modified

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000;
- 
+
+ //get apis
 app.get('/', function (req, res) {
-  res.send('Hello World i am Nikhil carpenter and hey')
+  res.send('Hello World ')
 })
+app.get('/login/', function (req, res) {
+  res.send('welcome to login page ')
+})
+app.get('/admin/', function (req, res) {
+  res.send('welcome to adminpanel ')
+})
+
+// post apis
+
+app.post('/', function (req, res) {
+  res.send('Hello World post')
+})
+app.post('/login/', function (req, res) {
+  res.send('welcome to login page post')
+})
+app.post('/admin/', function (req, res) {
+  res.send('welcome to admin panel post ')
+})
+
+
  
 app.listen(port,displayMessage)
 
